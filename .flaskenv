f19e0bc922a43e9459024d6cd164a9db1986c876
rainbowbird <kundy.zhang@gmail.com>
2019-03-07 00:25:24 +0800

    change api.py to app.py

---
+++ .flaskenv
@@ -0,0 +1,4 @@
+FLASK_RUN_HOST=0.0.0.0
+FLASK_RUN_PORT=80
+
+FLASK_ENV=development
--- app.py
+++ app.py
@@ -8,23 +8,22 @@ import hashlib
 import json
 import xml.etree.ElementTree as ET
 from flask import Flask, request, make_response
-from dispatcher import *
+import dispatcher
+import menu
 
 importlib.reload(sys)  # 不加这部分处理中文还是会出问题
 
 app = Flask(__name__)
 app.debug = True
 
-
 @app.route('/')  # 默认网址
 def index():
     return 'Index Page'
 
-
 @app.route('/wx', methods=['GET', 'POST'])
 def wechat_auth():
     '''
-    处理微信请求的处理函数，get方法用于认证，post方法取得微信转发的数据
+    微信请求的处理函数，GET方法用于认证，POST方法取得微信转发的数据
     '''
     if request.method == 'GET':
         token = 'kundy0808'
@@ -41,9 +40,10 @@ def wechat_auth():
         if (hashlib.sha1(s.encode('utf-8')).hexdigest() == signature):
             return make_response(echostr)
     else:
-        rec = request.stream.read()  # 接收消息
-        dispatcher = MsgDispatcher(rec)
-        data = dispatcher.dispatch()
+        rec = request.stream.read()
+        disp = dispatcher.MsgDispatcher(rec)
+        data = disp.dispatch()
+
         with open("./debug.log", "a") as file:
             file.write(data)
             file.close()
@@ -52,6 +52,10 @@ def wechat_auth():
         response.content_type = 'application/xml'
         return response
 
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=80)
+#if __name__ == '__main__':
+#    app.run(host="0.0.0.0", port=80)
+    #accessToken = Basic().get_access_token()
+    #myMenu = Menu()
+#    access_token = menu.get_access_token('wx25c644f06f4f6c68', 'f88f64ce280f6b686e17b52fc2d9066f')
+#    menu.create_menu(access_token)
 
